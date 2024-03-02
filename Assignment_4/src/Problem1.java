a0334832fd5fb035df43963d0c7d7cb13487285e
Abdoon Nur <abir023048@gmail.com>
2024-03-01 21:01:30 -0600

    Runtime Stack Diagram 3

--- Assignment_4/src/Problem1.java
+++ Assignment_4/src/Problem1.java
@@ -1,7 +1,7 @@
 public class Problem1 {
 
     public static void main(String[] args) {
-        int rev = reverseInt(143);
+        int rev = reverseInt(471);
         System.out.println(rev);
     }
 
